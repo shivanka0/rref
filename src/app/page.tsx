628d371b3f26667648a1da375119ebb19f890d1e
ShivaNKA0 <shiva.kanneboina@gmail.com>
2024-09-25 22:41:53 -0400

    Can take pic from camera now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,65 @@
+'use client';
+import { useEffect, useRef } from "react";
 import Link from "next/link";
 
 export default function HomePage() {
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const captureRef = useRef<HTMLButtonElement>(null);
+  const errorMsgRef = useRef<HTMLSpanElement>(null);
+
+  useEffect(() => {
+    const constraints = {
+      audio: false,
+      video: {
+        width: 640,
+        height: 480,
+      },
+    };
+
+    async function init() {
+      try {
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        handleSuccess(stream);
+      } catch (e) {
+        if (errorMsgRef.current) {
+          errorMsgRef.current.innerHTML = `navigator.getUserMedia.error:${e.toString()}`;
+        }
+      }
+    }
+
+    function handleSuccess(stream: MediaStream) {
+      if (videoRef.current) {
+        videoRef.current.srcObject = stream;
+      }
+    }
+
+    init();
+
+    if (captureRef.current && canvasRef.current && videoRef.current) {
+      const context = canvasRef.current.getContext('2d');
+      captureRef.current.addEventListener('click', () => {
+        if (context && videoRef.current) {
+          context.drawImage(videoRef.current, 0, 0, 640, 480);
+        }
+      });
+    }
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Hello World</h1>
+      {/* Webcam */}
+      <div className="video-wrap">
+        <video ref={videoRef} playsInline muted autoPlay></video>
+      </div>
+      {/* Capture */}
+      <div className="controller">
+        <button ref={captureRef}>Capture</button>
+      </div>
+      {/* Canvas */}
+      <canvas ref={canvasRef} width="640" height="480"></canvas>
+      {/* Error Message */}
+      <span ref={errorMsgRef}></span>
     </main>
   );
 }
